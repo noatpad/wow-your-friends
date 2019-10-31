68ecce82a07c2b16f1a5da064525e8ab37514de7
Daniel Hernandez <tamdannyhdz@gmail.com>
2019-10-30 23:24:14 -0600

    Style Footnote

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import styled from '@emotion/styled'
+import { keyframes } from '@emotion/core'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 2em;
+  padding: 3em 2em;
   background: #000b;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
 `
 
-const Icons = styled.div`
+const IconsDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -18,22 +25,56 @@ const Icons = styled.div`
 `
 
 const Icon = styled.i`
-  font-size: 1.5em;
-  padding: .25em;
-  margin: .25em;
+  font-size: 1.7em;
+  padding: .6em;
+  margin: .2em;
+  transition: all .4s;
+
+  &:hover {
+    transform: translateY(-20%);
+    color: ${props => props.color};
+  }
 `
 
-// TODO: Style footer further
-// IDEA: Add strawberry GIF in the center of the icons
-// TODO: Add tiny footnote on the bottom saying where the repo is and where it's hosted
-const Footer = () => (
-  <Container>
-    <Icons>
-      <Icon className="fab fa-twitter"/>
-      <Icon className="fab fa-github"/>
-    </Icons>
-    <p>A website made by a clueless danny</p>
-  </Container>
-)
+const float = keyframes`
+  to { transform: translateY(-20%); }
+`
+
+const GIF = styled.img`
+  height: 2.5em;
+  animation: ${float} 1s alternate infinite ease-in-out;
+`
+
+const Footnote = styled.p`
+  font-size: .7em;
+  color: #999;
+`
+
+// IDEA: Use react-spring once lag issue is resolved
+const Footer = () => {
+  const { file } = useStaticQuery(graphql`
+    query {
+      file(name: { eq: "strawberry" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <Container>
+      <IconsDiv>
+        <a href="https://twitter.com/aCluelessDanny" target="_blank" rel="noopener noreferrer">
+          <Icon color={"#38a1f3"} className="fab fa-twitter"/>
+        </a>
+        <GIF className="pixelated" src={file.publicURL} alt="A floating strawberry"/>
+        <a href="?" target="_blank" rel="noopener noreferrer">
+          <Icon color={"#6e5494"} className="fab fa-github"/>
+        </a>
+      </IconsDiv>
+      <p>A website made by a clueless danny</p>
+      <Footnote>Open sourced on GitHub | Hosted on ?</Footnote>
+    </Container>
+  )
+}
 
 export default Footer
